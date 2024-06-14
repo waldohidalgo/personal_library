0b276f4b1bc7d14c340a77abf43e9dd798bbda27
waldohidalgo <whidalgohp@gmail.com>
2024-06-14 17:11:05 -0400

    first commit

---
+++ public/client.js
@@ -0,0 +1,153 @@
+$(document).ready(function () {
+  let itemsRaw = [];
+
+  getData(itemsRaw, function (data) {
+    itemsRaw = data;
+  });
+
+  let comments = [];
+  $("#display").on("click", "li.bookItem", function () {
+    $("#detailTitle").html(
+      "<b>" +
+        itemsRaw[this.id].title +
+        "</b> (id: " +
+        itemsRaw[this.id]._id +
+        ")"
+    );
+
+    getComments(this.id, itemsRaw, function (data) {
+      comments = data;
+    });
+  });
+
+  $("#bookDetail").on("click", "button.deleteBook", function () {
+    $.ajax({
+      url: "/api/books/" + this.id,
+      type: "delete",
+      success: function (data) {
+        //update list
+        $("#detailComments").html(
+          '<p class="text-center  text-danger fw-bolder">' +
+            data +
+            "</p><a href='/'><button class='btn btn-primary d-block mx-auto'>Refresh the page</button></a>"
+        );
+        getData(itemsRaw, function (data) {
+          itemsRaw = data;
+        });
+      },
+    });
+  });
+
+  $("#bookDetail").on("click", "button.addComment", function () {
+    let newComment =
+      "<div class='text-center text-decoration-underline fw-bold'>New Comment: " +
+      $("#commentToAdd").val() +
+      "</div>";
+    $.ajax({
+      url: "/api/books/" + this.id,
+      type: "post",
+      dataType: "json",
+      data: $("#newCommentForm").serialize(),
+      success: function (data) {
+        comments.unshift(newComment); //adds new comment to top of list
+        $("#detailComments").html(comments.join(""));
+        getData(itemsRaw, function (data) {
+          itemsRaw = data;
+        });
+      },
+    });
+  });
+
+  $("#newBookForm").on("submit", function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/api/books",
+      type: "post",
+      dataType: "json",
+      data: $("#newBookForm").serialize(),
+      success: function (data) {
+        //update list
+        getData(itemsRaw, function (response) {
+          itemsRaw = response;
+        });
+        $("#newBookForm").trigger("reset");
+      },
+    });
+  });
+
+  $("#deleteAllBooks").click(function () {
+    $.ajax({
+      url: "/api/books",
+      type: "delete",
+      success: function (data) {
+        //update list
+        getData(itemsRaw, function (response) {
+          itemsRaw = response;
+        });
+      },
+    });
+  });
+  /*
+   *  For #sampleposting to update form action url to test inputs book id
+   */
+  $("#commentTest").submit(function () {
+    let id = $("#idinputtest").val();
+    $(this).attr("action", "/api/books/" + id);
+  });
+});
+
+function getComments(id, itemsRaw, cb) {
+  let comments = [];
+  $.getJSON("/api/books/" + itemsRaw[id]._id, function (data) {
+    comments = [];
+    $.each(data.comments, function (i, val) {
+      comments.push("<li class='ms-5'>" + (i + 1) + "." + val + "</li>");
+    });
+    comments.push(
+      '<br><form id="newCommentForm"><input type="text" class="form-control" id="commentToAdd" name="comment" placeholder="Add New Comment here"></form>'
+    );
+    comments.push(
+      '<br><div class="d-flex justify-content-center gap-2 flex-wrap"><button class="btn btn-success addComment" id="' +
+        data._id +
+        '">Add Comment</button>'
+    );
+    comments.push(
+      '<button class="btn btn-danger deleteBook" id="' +
+        data._id +
+        '">Delete Book</button></div>'
+    );
+    $("#detailComments").html(comments.join(""));
+    cb(comments);
+  });
+}
+
+function getData(itemsRaw, cb) {
+  $.getJSON("/api/books", function (data) {
+    let items = [];
+    itemsRaw = data;
+    $.each(data, function (i, val) {
+      items.push(
+        '<li class="bookItem mb-3" id="' +
+          i +
+          '">' +
+          val.title +
+          " - " +
+          val.commentcount +
+          " comments</li>"
+      );
+      return i !== 14;
+    });
+    if (items.length >= 15) {
+      items.push("<p>...and " + (data.length - 15) + " more!</p>");
+    }
+    $("#display").html(
+      "<h4 class='text-center mt-3 fw-bold text-decoration-underline py-3'>Books List</h4>"
+    );
+    $("<ul/>", {
+      class: "listWrapper",
+      html: items.join(""),
+    }).appendTo("#display");
+
+    cb(itemsRaw);
+  });
+}
---
+++ routes/api.js
@@ -0,0 +1,176 @@
+/*
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
+
+"use strict";
+const dbObject = require("../config/db.js");
+
+const { ObjectId } = require("mongodb");
+
+module.exports = function (app) {
+  const books = dbObject.db.collection("books");
+
+  app
+    .route("/api/books")
+    .get(async function (req, res) {
+      //response will be array of book objects
+      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+
+      try {
+        // retornar todos los books sin el field comment pero con un nuevo campo agregado igual a comment count siendo igual al largo del array comment
+
+        const result = await books
+          .aggregate([
+            {
+              $project: {
+                _id: 1,
+                title: 1,
+                commentcount: { $size: "$comments" },
+              },
+            },
+          ])
+          .toArray();
+        res.status(200).json(result);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+    })
+
+    .post(async function (req, res) {
+      let title = req.body.title;
+      //response will contain new book object including atleast _id and title
+
+      if (title === undefined || title === "") {
+        res.status(400).send("missing required field title");
+        return;
+      }
+
+      try {
+        const book = await books.findOneAndUpdate(
+          { _id: new ObjectId() },
+          { $setOnInsert: { title, comments: [] } },
+          { upsert: true, returnDocument: "after" }
+        );
+
+        res.status(200).json(book);
+      } catch (error) {
+        res.send(error);
+      }
+    })
+
+    .delete(async function (req, res) {
+      //if successful response will be 'complete delete successful'
+
+      try {
+        const result = await books.deleteMany({});
+
+        res.status(200).send("complete delete successful");
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
+    });
+
+  app
+    .route("/api/books/:id")
+    .get(async function (req, res) {
+      let bookid = req.params.id;
+      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      try {
+        const result = await books.findOne(
+          { _id: new ObjectId(String(bookid)) },
+          {
+            projection: { _id: 1, title: 1, comments: 1 },
+          }
+        );
+        if (result === null) {
+          throw new Error(
+            "input must be a 24 character hex string, 12 byte Uint8Array, or an integer"
+          );
+        }
+        res.status(200).json(result);
+      } catch (error) {
+        if (
+          error.message ===
+          "input must be a 24 character hex string, 12 byte Uint8Array, or an integer"
+        ) {
+          res.status(404).send("no book exists");
+          return;
+        }
+
+        res.status(500).send(error.message);
+      }
+    })
+
+    .post(async function (req, res) {
+      let bookid = req.params.id;
+      let comment = req.body.comment;
+      //json res format same as .get
+
+      try {
+        if (comment === undefined) {
+          res.status(400).send("missing required field comment");
+          return;
+        }
+        const result = await books.findOneAndUpdate(
+          { _id: new ObjectId(String(bookid)) },
+          { $push: { comments: comment } },
+          { returnDocument: "after" }
+        );
+        if (result === null) {
+          throw new Error("no book exists");
+        }
+        res.status(200).json(result);
+      } catch (error) {
+        if (
+          error.message ===
+          "input must be a 24 character hex string, 12 byte Uint8Array, or an integer"
+        ) {
+          res.status(404).send("no book exists");
+          return;
+        }
+        if (error.message === "no book exists") {
+          res.status(404).send("no book exists");
+          return;
+        }
+
+        res.status(500).send(error.message);
+      }
+    })
+
+    .delete(async function (req, res) {
+      let bookid = req.params.id;
+      //if successful response will be 'delete successful'
+
+      try {
+        if (bookid === undefined) {
+          res.status(400).send("no book exists");
+          return;
+        }
+        const result = await books.findOneAndDelete(
+          { _id: new ObjectId(String(bookid)) },
+          { projection: { _id: 1, title: 1, comments: 1 } }
+        );
+        if (result === null) {
+          throw new Error("no book exists");
+        }
+        res.status(200).send("delete successful");
+      } catch (error) {
+        if (
+          error.message ===
+          "input must be a 24 character hex string, 12 byte Uint8Array, or an integer"
+        ) {
+          res.status(404).send("no book exists");
+          return;
+        }
+        if (error.message === "no book exists") {
+          res.status(404).send("no book exists");
+          return;
+        }
+        res.status(500).send(error.message);
+      }
+    });
+};
