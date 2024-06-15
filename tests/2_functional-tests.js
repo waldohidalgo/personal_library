a7e0bc5a2e696eb896b5f07a4c051bfee3db7f19
waldohidalgo <whidalgohp@gmail.com>
2024-06-14 20:08:43 -0400

    comment example test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -18,31 +18,31 @@ suite("Functional Tests", function () {
    * ----[EXAMPLE TEST]----
    * Each test should completely test the response of the API end-point including response status code!
    */
-  test("#example Test GET /api/books", function (done) {
-    chai
-      .request(server)
-      .get("/api/books")
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isArray(res.body, "response should be an array");
-        assert.property(
-          res.body[0],
-          "commentcount",
-          "Books in array should contain commentcount"
-        );
-        assert.property(
-          res.body[0],
-          "title",
-          "Books in array should contain title"
-        );
-        assert.property(
-          res.body[0],
-          "_id",
-          "Books in array should contain _id"
-        );
-        done();
-      });
-  });
+  // test("#example Test GET /api/books", function (done) {
+  //   chai
+  //     .request(server)
+  //     .get("/api/books")
+  //     .end(function (err, res) {
+  //       assert.equal(res.status, 200);
+  //       assert.isArray(res.body, "response should be an array");
+  //       assert.property(
+  //         res.body[0],
+  //         "commentcount",
+  //         "Books in array should contain commentcount"
+  //       );
+  //       assert.property(
+  //         res.body[0],
+  //         "title",
+  //         "Books in array should contain title"
+  //       );
+  //       assert.property(
+  //         res.body[0],
+  //         "_id",
+  //         "Books in array should contain _id"
+  //       );
+  //       done();
+  //     });
+  // });
   /*
    * ----[END of EXAMPLE TEST]----
    */
